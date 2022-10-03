8bb7ccd89e1b42101962953b80dbd8ceaa2c8ed6
weloe <1345895607@qq.com>
2022-10-03 21:52:18 +0800

    Create main.go

---
+++ v2-context/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"go_web_framework/v2-context/mygin"
+	"net/http"
+)
+
+func main() {
+	r := mygin.New()
+	r.Get("/", func(c *mygin.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+	})
+	r.Get("/hello", func(c *mygin.Context) {
+		// expect /hello?name=123
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+	})
+
+	r.Post("/login", func(c *mygin.Context) {
+		c.JSON(http.StatusOK, mygin.H{
+			"username": c.PostForm("username"),
+			"password": c.PostForm("password"),
+		})
+	})
+
+	r.Run(":9999")
+}
